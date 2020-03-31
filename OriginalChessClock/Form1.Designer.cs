7d857acd1adb874a107b956db93774187c03f967
Justy Dejv <dejv.por@gmail.com>
2020-03-31 12:30:45 +0200

    Added my project

---
+++ OriginalChessClock/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace OriginalChessClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.wtptxt = new System.Windows.Forms.NumericUpDown();
+            this.startb = new System.Windows.Forms.Button();
+            this.wtpl = new System.Windows.Forms.Label();
+            this.btptxt = new System.Windows.Forms.NumericUpDown();
+            this.btpl = new System.Windows.Forms.Label();
+            this.tapttxt = new System.Windows.Forms.NumericUpDown();
+            this.taptl = new System.Windows.Forms.Label();
+            this.wtgl = new System.Windows.Forms.Label();
+            this.btgl = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.wtgtxt = new System.Windows.Forms.Label();
+            this.btgtxt = new System.Windows.Forms.Label();
+            this.ttxt = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.wtptxt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btptxt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tapttxt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // wtptxt
+            // 
+            this.wtptxt.Location = new System.Drawing.Point(264, 128);
+            this.wtptxt.Name = "wtptxt";
+            this.wtptxt.Size = new System.Drawing.Size(120, 20);
+            this.wtptxt.TabIndex = 0;
+            this.wtptxt.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // startb
+            // 
+            this.startb.Location = new System.Drawing.Point(480, 200);
+            this.startb.Name = "startb";
+            this.startb.Size = new System.Drawing.Size(144, 80);
+            this.startb.TabIndex = 1;
+            this.startb.Text = "Zapnout";
+            this.startb.UseVisualStyleBackColor = true;
+            this.startb.Click += new System.EventHandler(this.startb_Click);
+            // 
+            // wtpl
+            // 
+            this.wtpl.AutoSize = true;
+            this.wtpl.Location = new System.Drawing.Point(272, 104);
+            this.wtpl.Name = "wtpl";
+            this.wtpl.Size = new System.Drawing.Size(100, 13);
+            this.wtpl.TabIndex = 2;
+            this.wtpl.Text = "Čas bílý v minutách";
+            // 
+            // btptxt
+            // 
+            this.btptxt.Location = new System.Drawing.Point(424, 128);
+            this.btptxt.Name = "btptxt";
+            this.btptxt.Size = new System.Drawing.Size(120, 20);
+            this.btptxt.TabIndex = 3;
+            this.btptxt.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // btpl
+            // 
+            this.btpl.AutoSize = true;
+            this.btpl.Location = new System.Drawing.Point(432, 104);
+            this.btpl.Name = "btpl";
+            this.btpl.Size = new System.Drawing.Size(109, 13);
+            this.btpl.TabIndex = 4;
+            this.btpl.Text = "Čas černý v minutách";
+            // 
+            // tapttxt
+            // 
+            this.tapttxt.Location = new System.Drawing.Point(280, 232);
+            this.tapttxt.Name = "tapttxt";
+            this.tapttxt.Size = new System.Drawing.Size(120, 20);
+            this.tapttxt.TabIndex = 5;
+            this.tapttxt.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // taptl
+            // 
+            this.taptl.AutoSize = true;
+            this.taptl.Location = new System.Drawing.Point(248, 200);
+            this.taptl.Name = "taptl";
+            this.taptl.Size = new System.Drawing.Size(177, 13);
+            this.taptl.TabIndex = 6;
+            this.taptl.Text = "Přidávání času za tah v sekundách";
+            // 
+            // wtgl
+            // 
+            this.wtgl.AutoSize = true;
+            this.wtgl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.wtgl.Location = new System.Drawing.Point(112, 88);
+            this.wtgl.Name = "wtgl";
+            this.wtgl.Size = new System.Drawing.Size(97, 25);
+            this.wtgl.TabIndex = 8;
+            this.wtgl.Text = "Čas bílý";
+            this.wtgl.Visible = false;
+            // 
+            // btgl
+            // 
+            this.btgl.AutoSize = true;
+            this.btgl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btgl.Location = new System.Drawing.Point(568, 88);
+            this.btgl.Name = "btgl";
+            this.btgl.Size = new System.Drawing.Size(125, 25);
+            this.btgl.TabIndex = 10;
+            this.btgl.Text = "Čas černý ";
+            this.btgl.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // wtgtxt
+            // 
+            this.wtgtxt.AutoSize = true;
+            this.wtgtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.wtgtxt.Location = new System.Drawing.Point(88, 128);
+            this.wtgtxt.Name = "wtgtxt";
+            this.wtgtxt.Size = new System.Drawing.Size(151, 37);
+            this.wtgtxt.TabIndex = 11;
+            this.wtgtxt.Text = "00:00:00";
+            this.wtgtxt.Visible = false;
+            // 
+            // btgtxt
+            // 
+            this.btgtxt.AutoSize = true;
+            this.btgtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btgtxt.Location = new System.Drawing.Point(552, 128);
+            this.btgtxt.Name = "btgtxt";
+            this.btgtxt.Size = new System.Drawing.Size(151, 37);
+            this.btgtxt.TabIndex = 12;
+            this.btgtxt.Text = "00:00:00";
+            this.btgtxt.Visible = false;
+            // 
+            // ttxt
+            // 
+            this.ttxt.AutoSize = true;
+            this.ttxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ttxt.Location = new System.Drawing.Point(144, 344);
+            this.ttxt.Name = "ttxt";
+            this.ttxt.Size = new System.Drawing.Size(248, 37);
+            this.ttxt.TabIndex = 13;
+            this.ttxt.Text = "Na tahu je: Bílý";
+            this.ttxt.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ttxt);
+            this.Controls.Add(this.btgtxt);
+            this.Controls.Add(this.wtgtxt);
+            this.Controls.Add(this.btgl);
+            this.Controls.Add(this.wtgl);
+            this.Controls.Add(this.taptl);
+            this.Controls.Add(this.tapttxt);
+            this.Controls.Add(this.btpl);
+            this.Controls.Add(this.btptxt);
+            this.Controls.Add(this.wtpl);
+            this.Controls.Add(this.startb);
+            this.Controls.Add(this.wtptxt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.wtptxt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btptxt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tapttxt)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown wtptxt;
+        private System.Windows.Forms.Button startb;
+        private System.Windows.Forms.Label wtpl;
+        private System.Windows.Forms.NumericUpDown btptxt;
+        private System.Windows.Forms.Label btpl;
+        private System.Windows.Forms.NumericUpDown tapttxt;
+        private System.Windows.Forms.Label taptl;
+        private System.Windows.Forms.Label wtgl;
+        private System.Windows.Forms.Label btgl;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label wtgtxt;
+        private System.Windows.Forms.Label btgtxt;
+        private System.Windows.Forms.Label ttxt;
+    }
+}
+
---
+++ OriginalChessClock/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Author: David Porteš
+namespace OriginalChessClock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //wtg - white time game
+        //btg - black time game
+        //btp - black time pause
+        //wtp - white time pause
+        //tapt - time add per turn
+        //xxxl - label
+        //xxxtxt - textbox
+        //ttxt = Turn textbox
+
+
+        private void startb_Click(object sender, EventArgs e)
+        {
+            //hide buttons and unhide labels 
+            wtgl.Visible = true;
+            btgl.Visible = true;
+            btgtxt.Visible = true;
+            wtgtxt.Visible = true;
+            btpl.Visible = false;
+            btptxt.Visible = false;
+            wtpl.Visible = false;
+            wtptxt.Visible = false;
+            taptl.Visible = false;
+            tapttxt.Visible = false;
+            startb.Visible = false;
+            ttxt.Visible = true;
+
+            //Timer for white
+            counterW = Convert.ToInt32(wtptxt.Value) * 60;           
+            timer1.Interval = 1000; // 1 second
+            TimeSpan counterW2 = TimeSpan.FromSeconds(counterW);
+            wtgtxt.Text = counterW2.ToString(@"hh\:mm\:ss");
+
+            //Timer for black
+            counterB = Convert.ToInt32(btptxt.Value) * 60;
+            timer2.Interval = 1000; // 1 second
+            TimeSpan counterB2 = TimeSpan.FromSeconds(counterB);
+            btgtxt.Text = counterB2.ToString(@"hh\:mm\:ss");
+
+            //Unfocus button, so you  can press space and game will start
+            this.Focus();
+        }
+        //WHITE TIMER TICK
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //-1s in timer
+            counterW--;
+            //update timer with each tick
+            TimeSpan counterW2 = TimeSpan.FromSeconds(counterW);
+            wtgtxt.Text = counterW2.ToString(@"hh\:mm\:ss");
+            //when timer hits 0, stop timers and show Lost message
+            if (counterW == 0)
+            {
+                timer1.Stop();
+                timer2.Stop();
+                MessageBox.Show("Černý vyhrál, bílému došel čas");
+            }
+
+        }
+
+        //BLACK TIMER TICK
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //-1s in timer
+            counterB--;
+            //update timer with each tick
+            TimeSpan counterB2 = TimeSpan.FromSeconds(counterB);
+            btgtxt.Text = counterB2.ToString(@"hh\:mm\:ss");
+            //when timer hits 0, stop timers and show Lost message
+            if (counterB == 0)
+            {
+                timer1.Stop();
+                timer2.Stop();
+                MessageBox.Show("Bílý vyhrál, černýmu došel čas");
+            }
+
+        }
+        int counterW; // preset time from user for White
+        int counterB;// preset time from user for black            
+        string turn = "white"; // White = White, Black = Black
+        int pause = 0; //0= play, 1 = pause
+        int start = 0;// 0=game has not started, 1= Game started
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {                     
+            //When you press space you swap turns
+            if (e.KeyCode == Keys.Space)
+            {
+                //preventing start game getting X s for starting game 
+                if (start == 0)
+                {
+                        timer1.Start();
+                        start = 1;
+                        turn = "white";
+                }
+                else
+                {
+                    //IF TURN == 0 [If it´s white turn when you press clock] swap turn to Black -> White pressing clocks and Black timer starting
+                    if (turn == "white")
+                    {
+
+                        timer1.Stop();
+                        timer2.Start();
+                        turn = "black";
+                        counterW += Convert.ToInt32(tapttxt.Value);
+                        TimeSpan counterW2 = TimeSpan.FromSeconds(counterW);
+                        wtgtxt.Text = counterW2.ToString(@"hh\:mm\:ss");
+
+                    }
+                    //Else it has to be TURN black and swap turn to White -> Black pressing clock and White timer starting
+                    else
+                    {
+                        timer2.Stop();
+                        timer1.Start();
+                        turn = "white";
+                        counterB += Convert.ToInt32(tapttxt.Value);
+                        TimeSpan counterB2 = TimeSpan.FromSeconds(counterB);
+                        btgtxt.Text = counterB2.ToString(@"hh\:mm\:ss");
+                    }
+                }
+                //If turn is white, write into txt "Na tahu je bílý"
+                if(turn == "white")
+                {
+                   ttxt.Text = "Na tahu je bílý";
+                }
+                //Else turn is black, write into txt "Na tahu je černý"
+                else
+                {
+                    ttxt.Text = "Na tahu je černý";
+                }
+            }
+            //Pause game
+            if(e.KeyCode == Keys.P)
+            { //If game is not paused, pause timers and show message "Hra pozastavena"
+                if(pause==0)
+                {
+                    timer1.Stop();
+                    timer2.Stop();
+                    MessageBox.Show("Hra pozastavena");
+                    pause = 1;
+                }
+                //if game is paused 
+                else
+                {//and it was White turn, start timer for white
+                    if (turn == "white")
+                    {
+                        timer1.Start();
+                    }
+                    //and it was Black turn, start timer for black
+                    else
+                    {
+                        timer2.Start();
+                    }
+                    pause = 0;
+                }
+            }
+        }
+    }
+}
+
